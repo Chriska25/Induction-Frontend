b28934e79ad9f543ff9a3a767490313f9fcb37d5
Chriska25 <musakanechris@gmail.com>
2026-01-21 16:35:51 +0200

    Update client.ts

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,6 @@
 import { RegistrationData } from '../types';
 
-const API_URL = '/api';
+const API_URL = (import.meta.env.VITE_API_URL || '') + '/api';
 
 export const api = {
     // Check backend health
